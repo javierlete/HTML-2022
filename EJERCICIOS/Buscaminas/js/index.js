787fac56eacddb5fb979013cdd69c64b65f9d0f2
Javier Lete <javier@eskura21.com>
2022-12-20 15:58:51 +0100

    Buscaminas funcional

--- EJERCICIOS/Buscaminas/js/index.js
+++ EJERCICIOS/Buscaminas/js/index.js
@@ -2,7 +2,32 @@ const NUMERO_FILAS = 8;
 const NUMERO_COLUMNAS = 8;
 const NUMERO_MINAS = 8;
 
+const VACIO = '~';
+const MINA = '*';
+const EXPLOSION = '*';
+
 let explosion;
+const botones = [];
+
+const mapa = [];
+for (let y = 0; y < NUMERO_FILAS + 2; y++) {
+    mapa[y] = [];
+
+    for (let x = 0; x < NUMERO_COLUMNAS + 2; x++) {
+        mapa[y][x] = VACIO;
+    }
+}
+
+for (let i = 0; i < NUMERO_MINAS; i++) {
+    const x = aleatorio(1,8);
+    const y = aleatorio(1,8);
+
+    if(mapa[y][x] === MINA) {
+        i--;
+    } else {
+        mapa[y][x] = MINA;
+    }
+}
 
 window.addEventListener('DOMContentLoaded', function () {
     const tablero = document.querySelector('#tablero');
@@ -11,6 +36,8 @@ window.addEventListener('DOMContentLoaded', function () {
     let boton;
 
     for (let y = 1; y <= NUMERO_FILAS; y++) {
+        botones[y] = [];
+
         for (let x = 1; x <= NUMERO_COLUMNAS; x++) {
             boton = document.createElement('button');
 
@@ -20,34 +47,47 @@ window.addEventListener('DOMContentLoaded', function () {
             boton.addEventListener('click', botonPulsado);
 
             tablero.appendChild(boton);
+
+            botones[y][x] = boton;
         }
-    }
-
-    const botones = document.querySelectorAll('#tablero > button');
-
-    for (let i = 0; i < NUMERO_MINAS; i++) {
-        botones[aleatorio(0, NUMERO_FILAS * NUMERO_COLUMNAS - 1)].dataset.contenido = '*';
     }
 });
 
-function botonPulsado() {
-    this.style.visibility = 'hidden';
+function botonPulsado(e) {
+    const boton = e.target
+    boton.style.visibility = 'hidden';
+    
+    const x = parseInt(boton.style.gridColumnStart);
+    const y = parseInt(boton.style.gridRowStart);
 
     const div = document.createElement('div');
 
     div.style.textAlign = 'center';
     div.style.verticalAlign = 'center';
 
-    div.style.gridColumnStart = this.style.gridColumnStart;
-    div.style.gridRowStart = this.style.gridRowStart;
+    div.style.gridColumnStart = x;
+    div.style.gridRowStart = y;
 
-    if (this.dataset.contenido === '*') {
-        div.innerHTML = '*'
+    if (mapa[y][x] === MINA) {
+        div.innerHTML = EXPLOSION;
         explosion.play();
     } else {
-        div.innerHTML = '~';
+        div.innerHTML = visualizarPistas(x, y);
     }
 
     tablero.appendChild(div);
+}
 
+function visualizarPistas(x, y) {
+    let minas = 0;
+
+    for (let minasY = y - 1; minasY <= (y + 1); minasY++) {
+        for (let minasX = x - 1; minasX <= (x + 1); minasX++) {
+            if (mapa[minasY][minasX] === MINA) {
+                minas++;
+            }
+        }
+    }
+
+    return minas ? minas : ' ';
 }
